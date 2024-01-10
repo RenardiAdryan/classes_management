eee63ea5464452517c94a11611b5d1f7287cdb4e
Renardi <renardi98@gmail.com>
2024-01-10 21:35:20 +0700

    Update

--- classmanagement/core/views.py
+++ classmanagement/core/views.py
@@ -19,7 +19,9 @@ def login_view(request):
         user_auth = authenticate(request=request, email=email, password=password)
         if user_auth:
             login(request, user_auth)
-        return redirect('core:home')
+            return redirect('core:home')
+        else:
+            return render(request, 'login.html')
     
 def logout_view(request):
     logout(request)
@@ -39,12 +41,9 @@ def home(request):
     else:
 
         if 'create-classes' in request.POST:
-
             name = request.POST.get('name',None)
             teacher_assignee = request.POST.get('assignee',None)
-   
             user = User.objects.get(id=teacher_assignee)
-
             Classes.objects.create(
                 name = name,
                 teacher = user.teacher
@@ -74,5 +73,15 @@ def class_detail(request,id):
             classes.teacher = user.teacher
             classes.save()
 
-
+        elif 'delete-classes' in request.POST:
+            classes = Classes.objects.get(
+                id=id
+            ).delete()
+        elif 'delete-student' in request.POST:
+            student_id = request.POST.get('student',None)
+            user = User.objects.get(id=student_id)
+            classes = Classes.objects.get(
+                id=id
+            )
+            classes.student.remove(user)
         return redirect('core:home') 
